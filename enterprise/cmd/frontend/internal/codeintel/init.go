c4dc36302452b0dec5f7db8904096468bc46f0a4
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-02-04 22:00:29 +0000

    wip

--- enterprise/cmd/frontend/internal/codeintel/init.go
+++ enterprise/cmd/frontend/internal/codeintel/init.go
@@ -66,7 +66,7 @@ func newResolver(ctx context.Context, db database.DB, observationContext *observ
 		db,
 	)
 
-	return codeintelgqlresolvers.NewResolver(db, innerResolver, &observation.Context{Sentry: observationContext.Sentry}), nil
+	return codeintelgqlresolvers.NewResolver(services.dbStore, services.gitserverClient, innerResolver, &observation.Context{Sentry: observationContext.Sentry}), nil
 }
 
 func newUploadHandler(services *Services) func(internal bool) http.Handler {
