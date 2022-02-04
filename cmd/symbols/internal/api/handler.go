bf4a5796851d815a27db023d84c43699d5449765
Eric Fritz <eric@eric-fritz.com>
2022-02-04 17:10:15 -0600

    Split symbols into OSS/enterprise (aka The Great Symbols Schism TM).

--- cmd/symbols/internal/api/handler.go
+++ cmd/symbols/internal/api/handler.go
@@ -7,12 +7,12 @@ import (
 
 	"github.com/inconshreveable/log15"
 
-	"github.com/sourcegraph/sourcegraph/cmd/symbols/internal/types"
+	sharedtypes "github.com/sourcegraph/sourcegraph/cmd/symbols/shared/types"
 	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 func NewHandler(
-	searchFunc types.SearchFunc,
+	searchFunc sharedtypes.SearchFunc,
 ) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/search", handleSearchWith(searchFunc))
@@ -22,9 +22,9 @@ func NewHandler(
 
 const maxNumSymbolResults = 500
 
-func handleSearchWith(searchFunc types.SearchFunc) func(w http.ResponseWriter, r *http.Request) {
+func handleSearchWith(searchFunc sharedtypes.SearchFunc) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var args types.SearchArgs
+		var args sharedtypes.SearchArgs
 		if err := json.NewDecoder(r.Body).Decode(&args); err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
